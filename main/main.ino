70716b82425a14f7086b9c12108008f945a8d641
katapultman <katapultman@abv.bg>
2020-05-31 20:49:28 +0300

    Added basic walk cycle for one leg

--- main/main.ino
+++ main/main.ino
@@ -1,33 +1,3 @@
- /*
- * Original sourse: https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library
- * This is the Arduino code PAC6985 16 channel servo controller
- * watch the video for details and demo http://youtu.be/y8X9X10Tn1k
- *  * 
- * Written by Ahmad Nejrabi for Robojax Video channel www.Robojax.com
- * Date: Dec 15, 2017, in Ajax, Ontario, Canada
- * Permission granted to share this code given that this
- * note is kept with the code.
- * Disclaimer: this code is "AS IS" and for educational purpose only.
- * 
- */
-/*************************************************** 
-  This is an example for our Adafruit 16-channel PWM & Servo driver
-  Servo test - this will drive 16 servos, one after the other
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/815
-
-  These displays use I2C to communicate, 2 pins are required to  
-  interface. For Arduino UNOs, thats SCL -> Analog 5, SDA -> Analog 4
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
@@ -40,15 +10,10 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // want these to be as small/large as possible without hitting the hard stop
 // for max range. You'll have to tweak them as necessary to match the servos you
 // have!
-#define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
-#define SERVO_COUNT 2
-#define ANGLE_MARGIN 20
+#define SERVOMIN 125 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 575 // this is the 'maximum' pulse length count (out of 4096)
+#define LEG_COUNT 1 // TODO: Change to 2 legs
 #define MAX_ANGLE 180
-#define SERVO_LOOPS 5
-
-// our servo # counter
-uint8_t servonum = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -60,8 +25,19 @@ void setup() {
 }
 
 int angle_to_pulse(int angle) {
-  return map(angle, 0, 180, SERVOMIN, SERVOMAX);
+  return map(angle, 0, MAX_ANGLE, SERVOMIN, SERVOMAX);
   // maps from degree out of 0 to 180 and to 125 to 575
+}
+
+void walk_cycle(int servo1, int servo2) {
+  pwm.setPWM(servo1, 0, angle_to_pulse(45));
+  delay(4000);
+  pwm.setPWM(servo2, 0, angle_to_pulse(45));
+  delay(4000);
+  pwm.setPWM(servo1, 0, angle_to_pulse(90));
+  delay(4000);
+  pwm.setPWM(servo2, 0, angle_to_pulse(90));
+  delay(4000); 
 }
 
 // the code inside loop() has been updated by Robojax
@@ -70,17 +46,14 @@ void loop() {
 
   // 125 - 0 degrees
   // 575 - 180 degrees
-  for(int servo = 0; servo < SERVO_COUNT; servo++) {
-    for(int i = 0; i < SERVO_LOOPS; i++) {
-      int angl = random(0, 180);
-      int converted_pulse = angle_to_pulse(angl);
+  for(int servo = 0; servo < LEG_COUNT; servo += 2) { // go by 2 servos
+      // int angl = random(0, 180);
+      // int converted_pulse = angle_to_pulse(90);
       // pwm.setPWM(servo, 0, SERVOMIN);
-      delay(500);
-      pwm.setPWM(servo, 0, converted_pulse);
-      delay(500);
+      walk_cycle(servo, servo + 1);
      //  pwm.setPWM(servo, 0, SERVOMAX);
      // delay(500); 
-    }
+    // }
   }
 
  
